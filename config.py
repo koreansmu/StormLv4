af1e7e4ec883e514fb56e2b78f877674f4eac026
koreansmu <shuklakusum4q@gmail.com>
2025-01-17 16:14:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/kxunal/Lv2",
+    "https://github.com/koreansmu/StormLv4",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "mainr")
 GIT_TOKEN = getenv(
